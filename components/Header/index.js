1048bd04f90d3fa1b34994cd7f42211847abdb64
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-10-04 08:42:09 -0700

    got header and tabs done... I think?

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+    const degree = document.createElement('sup');
+
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp')
+
+    const currentDate = new Date();
+    date.textContent = currentDate.toDateString().slice(4)
+    title.textContent = 'Lambda Times';
+    degree.textContent = 'o';
+    temp.textContent = '98';
+    
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(title);
+    headerDiv.appendChild(temp);
+    temp.appendChild(degree);
+    
+    return headerDiv
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
+
